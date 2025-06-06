9c55ac3aa8dea2eb815eb86520e70e59dc3e75a5
Laura Barrero Ramirez <laurabarrero295@gmail.com>
2025-06-05 21:57:46 -0500

    creacion de front

---
+++ front/src/components/Boton.jsx
@@ -0,0 +1,15 @@
+import { Button } from "@mui/material";
+
+function Boton() {
+
+
+  return (
+    <>
+      <div>
+        <h2>TOCA AQUI</h2>
+      </div>
+    </>
+  )
+}
+
+export default Boton;
