ea70b5f37ded60b5b8e2346cb20f858592d8a04b
calvin howard <calvin.uhh@gmail.com>
2025-06-05 22:09:44 -0500

    Modificando boton de Material para ejemplo

--- front/src/components/Boton.jsx
+++ front/src/components/Boton.jsx
@@ -1,15 +1,28 @@
 import { Button } from "@mui/material";
+import { useState } from "react";
 
 function Boton() {
-
+  const [stateButton, setStateButton] = useState("none");
 
   return (
     <>
       <div>
-        <h2>TOCA AQUI</h2>
+        <Button
+          onClick={() => {
+            if (stateButton === "none") {
+              setStateButton("block");
+            } else {
+              setStateButton("none");
+            }
+          }}
+        >
+          <h2>TOCA AQUI</h2>
+        </Button>
+
+        <h1 style={{ display: stateButton }}>Titulo para probar</h1>
       </div>
     </>
-  )
+  );
 }
 
 export default Boton;
