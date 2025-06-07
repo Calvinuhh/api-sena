bdd9f8ad9fdbf4d6ee1b8eb78bea9fcc2525e46f
jesus ramos yanez <jesus.ramos097@outlook.es>
2025-06-06 20:27:07 -0500

    actualizados controllers, database y routers

--- controllers.js
+++ controllers.js
@@ -15,3 +15,13 @@ export const crearUsuario = async (req, res) => {
     return res.status(400).json({ error: error.message });
   }
 };
+
+
+export const obtenerUsuarios = async (req, res) => {
+  try{
+    const usuarios = await Usuario.findAll();
+    res.status(200).json(usuarios);
+  } catch (error){
+    res.status(500).json({error: "Error al obtener los usuarios"});
+  }
+}
--- routes.js
+++ routes.js
@@ -1,12 +1,15 @@
 import { Router } from "express";
-import { crearUsuario } from "./controllers.js";
+import { crearUsuario, obtenerUsuarios } from "./controllers.js";
 
 const router = Router();
 
 router.post("/usuarios", crearUsuario); // CREATE ser        ---> Calvin
 // router.get("/usuarios")                  // GET all users     ---> Jesus
+router.get("/usuarios", obtenerUsuarios);
 // router.get("/usuarios/:id");             // GET user by ID    ---> Henry
 // router.put("/usuarios/:id")              // UPDATE user by ID ---> Laura
 // router.delete("/usuarios/:id");          // DELETE user by ID ---> Calvin
 
 export default router;
+
+
